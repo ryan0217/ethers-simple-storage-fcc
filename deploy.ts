58787ac7743f5c9d280db9d80741fa5663d741c9
sunrui1996 <183428323@qq.com>
2022-10-10 23:15:14 +0800

    use typescript

--- deploy.ts
+++ deploy.ts
@@ -1,10 +1,10 @@
-const ethers = require("ethers")
-const fs = require("fs")
-require("dotenv").config()
+import { ethers } from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY!, provider)
   const abi = fs.readFileSync("./SimpleStorage.abi", "utf-8")
   const binary = fs.readFileSync("./SimpleStorage.bin", "utf-8")
   const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
